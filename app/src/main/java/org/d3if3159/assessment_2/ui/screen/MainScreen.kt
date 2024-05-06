65671695d608ab4a5fb08d3a1bb2b5fa884f364c
Mariana <marmar.smsr@gmail.com>
2024-05-06 22:37:16 +0700

    Membuat Item Dapat Diklik

--- app/src/main/java/org/d3if3159/assessment_2/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3159/assessment_2/ui/screen/MainScreen.kt
@@ -1,6 +1,8 @@
 package org.d3if3159.assessment_2.ui.screen
 
 import android.content.res.Configuration
+import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -52,7 +55,7 @@ fun ScreenContent(modifier: Modifier) {
 
     val viewModel: MainViewModel = viewModel()
     val dataMusik = viewModel.dataMusik
-//    val dataMusik = emptyList<Musik>()
+    val context = LocalContext.current
 
     if (dataMusik.isEmpty()) {
         Column(
@@ -70,7 +73,10 @@ fun ScreenContent(modifier: Modifier) {
             modifier = modifier.fillMaxSize()
         ) {
             items(dataMusik) {
-                ListItem(musik = it)
+                ListItem(musik = it) {
+                    val pesan = context.getString(R.string.x_diklik, it.judul)
+                    Toast.makeText(context, pesan, Toast.LENGTH_SHORT).show()
+                }
                 Divider()
             }
         }
@@ -78,10 +84,11 @@ fun ScreenContent(modifier: Modifier) {
 }
 
 @Composable
-fun ListItem(musik: Musik) {
+fun ListItem(musik: Musik, onClick: () -> Unit) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
+            .clickable { onClick() }
             .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
